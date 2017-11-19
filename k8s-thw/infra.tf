f1900530eac63098a42cca19cd5b122048c296fd
Luke Siler <lukesiler@gmail.com>
2017-11-19 08:16:10 -0800

    nodes subnet & internal firewall rule

--- k8s-thw/infra.tf
+++ k8s-thw/infra.tf
@@ -2,10 +2,18 @@ variable "env" {
   default = "siler-k8s-thw"
 }
 
+variable "region" {
+  default = "us-west1"
+}
+
+variable "cidr-nodes" {
+  default = "10.240.0.0/24"
+}
+
 provider "google" {
   credentials = "${file("../../secrets/gcp/***REMOVED***-default/***REMOVED*** GCS 7531 ***REMOVED*** Prj Blue ***REMOVED***-437967ffb3d7.json")}"
   project     = "***REMOVED***-gcs-7531-***REMOVED***-prj-***REMOVED***-***REMOVED***"
-  region      = "us-west1"
+  region      = "${var.region}"
 }
 
 resource "google_compute_network" "net" {
@@ -20,4 +28,30 @@ output "net-gtwy" {
 
 output "net-self_link" {
   value = "${google_compute_network.net.self_link}"
+}
+
+resource "google_compute_subnetwork" "subnet-nodes" {
+  name          = "${var.env}-nodes"
+  ip_cidr_range = "${var.cidr-nodes}"
+  network       = "${google_compute_network.net.self_link}"
+  region        = "${var.region}"
+}
+
+resource "google_compute_firewall" "allow-internal" {
+  name    = "${var.env}-allow-internal"
+  network = "${google_compute_network.net.self_link}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+  }
+
+  allow {
+    protocol = "udp"
+  }
+
+  source_ranges = ["10.240.0.0/24", "10.200.0.0/16"]
 }
