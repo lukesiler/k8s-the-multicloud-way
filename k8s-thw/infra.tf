99cd4e2337f17fc8d80d27fecce0e68f29acd0d5
Luke Siler <lukesiler@gmail.com>
2017-11-19 08:21:07 -0800

    external firewall rule

--- k8s-thw/infra.tf
+++ k8s-thw/infra.tf
@@ -55,3 +55,19 @@ resource "google_compute_firewall" "allow-internal" {
 
   source_ranges = ["10.240.0.0/24", "10.200.0.0/16"]
 }
+
+resource "google_compute_firewall" "allow-external" {
+  name    = "${var.env}-allow-external"
+  network = "${google_compute_network.net.self_link}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports = ["22", "6443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
