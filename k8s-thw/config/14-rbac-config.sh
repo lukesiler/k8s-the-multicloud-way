5a8fb0fe5007d1e7a3afd9431db627fad1026d1f
Luke Siler <lukesiler@gmail.com>
2017-11-26 08:40:27 -0800

    apiserver -> kubelet role

---
+++ k8s-thw/config/14-rbac-config.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+gcloud compute ssh siler-k8s-thw-0
+
+# create kube-apiserver-to-kubelet role to allow apiserver to connect to kubelets
+cat <<EOF | kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRole
+metadata:
+  annotations:
+    rbac.authorization.kubernetes.io/autou***REMOVED***ate: "true"
+  labels:
+    kubernetes.io/bootstrapping: rbac-defaults
+  name: system:kube-apiserver-to-kubelet
+rules:
+  - apiGroups:
+      - ""
+    resources:
+      - nodes/proxy
+      - nodes/stats
+      - nodes/log
+      - nodes/spec
+      - nodes/metrics
+    verbs:
+      - "*"
+EOF
+
+# bind kubernetes user to the role
+cat <<EOF | kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: system:kube-apiserver
+  namespace: ""
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: system:kube-apiserver-to-kubelet
+subjects:
+  - apiGroup: rbac.authorization.k8s.io
+    kind: User
+    name: kubernetes
+EOF
