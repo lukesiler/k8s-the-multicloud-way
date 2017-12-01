ab9c9c02efd69df9aaea15f6fbfed15f0c852e2d
Luke Siler <lukesiler@gmail.com>
2017-11-30 17:21:58 -0800

    something

--- k8s-thw/infra.tf
+++ k8s-thw/infra.tf
@@ -176,17 +176,23 @@ resource "google_compute_target_pool" "master-node-pool" {
   name = "${var.env}-masters-pool"
 
   instances = [
-    "${google_compute_instance.master-nodes.self_link}
+    "${google_compute_instance.master-nodes.*.self_link}"
   ]
 
-  health_checks = [
-    "${google_compute_http_health_check.default.name}",
-  ]
+  // add health check in future
 }
 
+/*
 resource "google_compute_http_health_check" "default" {
   name               = "default"
   request_path       = "/"
   check_interval_sec = 1
   timeout_sec        = 1
+}
+*/
+
+resource "google_compute_forwarding_rule" "default" {
+  name       = "${var.env}-forwarding-rule"
+  target     = "${google_compute_target_pool.master-node-pool.self_link}"
+  port_range = "6443"
 }
