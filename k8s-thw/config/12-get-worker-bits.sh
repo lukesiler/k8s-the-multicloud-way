6333778b3b436f78ef555e1eb1a941c8d284d59e
Luke Siler <lukesiler@gmail.com>
2018-01-23 09:38:49 -0800

    upgrade to 1.9

--- k8s-thw/config/12-get-worker-bits.sh
+++ k8s-thw/config/12-get-worker-bits.sh
@@ -7,7 +7,7 @@ VER_CNI=${3}
 
 wget -q --show-progress --https-only --timestamping \
   https://github.com/containernetworking/plugins/releases/download/v${VER_CNI}/cni-plugins-amd64-v${VER_CNI}.tgz \
-  https://github.com/kubernetes-incubator/cri-containerd/releases/download/v${VER_CONTAINERD}/cri-containerd-${VER_CONTAINERD}.tar.gz \
+  https://github.com/kubernetes-incubator/cri-containerd/releases/download/v${VER_CONTAINERD}/cri-containerd-${VER_CONTAINERD}.linux-amd64.tar.gz \
   https://storage.googleapis.com/kubernetes-release/release/v${VER_K8S}/bin/linux/amd64/kubectl \
   https://storage.googleapis.com/kubernetes-release/release/v${VER_K8S}/bin/linux/amd64/kube-proxy \
   https://storage.googleapis.com/kubernetes-release/release/v${VER_K8S}/bin/linux/amd64/kubelet
--- k8s-thw/config/13-install-worker-bits.sh
+++ k8s-thw/config/13-install-worker-bits.sh
@@ -9,9 +9,9 @@ sudo mkdir -p \
   /var/lib/kubernetes \
   /var/run/kubernetes
 
-sudo tar -xvf cni-plugins-amd64-v0.6.0.tgz -C /opt/cni/bin/
+sudo tar -xvf cni-plugins-amd64-v*.tgz -C /opt/cni/bin/
 
-sudo tar -xvf cri-containerd-1.0.0-alpha.0.tar.gz -C /
+sudo tar -xvf cri-containerd-*.tar.gz -C /
 
 chmod +x kubectl kube-proxy kubelet
 
